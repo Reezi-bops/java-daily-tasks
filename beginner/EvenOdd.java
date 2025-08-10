c757fd12df6df7a010145c8f06c32bbb228917e7
Raiko Estrada <jones.estrada3@gmail.com>
2025-08-10 12:00:51 +0800

    day3

---
+++ beginner/EvenOdd.java
@@ -0,0 +1,15 @@
+package beginner;
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n1 = sc.nextInt();
+
+        System.out.println(n1 == 2 ? "Even" : "Odd");
+
+        sc.close();
+    }
+}

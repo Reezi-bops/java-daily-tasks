4af973430a36c069bc0c9f04dd3c0ba4a044fb1f
Raiko Estrada <jones.estrada3@gmail.com>
2025-08-15 21:33:20 +0800

    day4

--- beginner/FactorialNumber.java
+++ beginner/FactorialNumber.java
@@ -10,8 +10,6 @@ public class FactorialNumber {
         sc.close();
 
         long factorial = 1;
-
-
         if (num < 0) {
             System.out.println("Factorial is not defined for negative numbers.");
         } else {
